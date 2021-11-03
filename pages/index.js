647ba792b467abe58b6c2dfc658d9c5cbde74a36
Minger Lin <mingerlin1001@berkeley.edu>
2021-11-02 22:43:03 -0700

    finish tutorial 9-11

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log("placing your order");
+    router.push("/product");
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/product">
+        <a>Product</a>
+      </Link>
+      <button onClick={handleClick}>Place Order</button>
+    </div>
+  );
 }
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,27 @@
+import Link from "next/link";
+
 function ProductList() {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <h2>
+        <Link href="/product/1">
+          <a>product1</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/product/2">
+          <a>product2</a>
+        </Link>
+      </h2>
+      <h2>
+        {/* replace will take you back to home page when you click reverse in chrom browser */}
+        <Link href="/product/3" replace>
+          <a>product3</a>
+        </Link>
+      </h2>
     </>
   );
 }
